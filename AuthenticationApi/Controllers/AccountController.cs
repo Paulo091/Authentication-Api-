9f18825034ba399f1ded8b99fbaad74eaa41dea6
p.dias <p.dias@vocalcom.com>
2021-04-29 20:30:09 -0300

    Initial

---
+++ AuthenticationApi/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("Register")]
+        public async Task<object> Register()
+        {
+            return "";
+        }
+
+        [HttpPost("Login")]
+        public async Task<object> Login()
+        {
+            return "";
+        }
+
+    }
+}
